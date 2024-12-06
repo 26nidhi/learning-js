fbf14188cb4c64845ab0b462c43ba95e445751c0
NIDHI WALKE <bcs_2023041@iiitm.ac.in>
2024-12-06 07:28:58 +0000

    comparision

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,22 +1,37 @@
-let score="abc"
+// let score="abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber =Number(score)
+// let valueInNumber =Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let s=true
+// let s=true
 
-console.log(typeof s)
-console.log(typeof(s))
+// console.log(typeof s)
+// console.log(typeof(s))
 
-let ValueInNumber =Number(s)
+// let ValueInNumber =Number(s)
 
-console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
 
 //"" converted to false
 //"nidhi" converted to true
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+// console.log(true+); error
+
+// console.log(null >0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+
+
