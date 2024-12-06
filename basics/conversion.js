e3c1846845186c086b6f3226743f7e6af96f7078
NIDHI WALKE <bcs_2023041@iiitm.ac.in>
2024-12-06 06:36:14 +0000

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,22 @@
+let score="abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber =Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let s=true
+
+console.log(typeof s)
+console.log(typeof(s))
+
+let ValueInNumber =Number(s)
+
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//"" converted to false
+//"nidhi" converted to true
---
+++ basics/strict.js
@@ -0,0 +1,19 @@
+//node basics/strict.js
+//chmod +x basics/strict.js
+//data types
+/*
+number
+bigint
+string
+boolean
+null->standalone value
+undefined
+symbol ->unique
+*/
+
+/*
+object
+typeof
+null(object)
+*/
+console.log( typeof 6+4);
