39df18ce7eabd4617839f21f11d53af064544915
NIDHI WALKE <bcs_2023041@iiitm.ac.in>
2024-12-07 01:10:01 +0000

    math functions

---
+++ basics/memory.js
@@ -0,0 +1,35 @@
+// two types of memory in javascript
+// stack and heaps
+// stack(primitive) copy is aasigned
+//heap(non-primitive) reference is given
+
+// let user1={ age:32,city:"jaipur"}
+// let user2=user1;
+
+// console.log(user2);
+
+// user2.city="bombay";
+
+// console.log(user1);
+
+const game =new String('subwaySurfers');
+
+// console.log(game);
+// console.log(game[2]);
+
+// many string methods like : trim , slice , substring , replace , includes
+
+// Numbers and Math
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
